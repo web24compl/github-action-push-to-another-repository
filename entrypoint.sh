4fb24359a45477f3eda48d4d21de968ca6761f9b
tomaszperkowski <91724331+tomaszperkowski@users.noreply.github.com>
2022-12-21 09:04:11 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -54,9 +54,6 @@ else
 	exit 1
 fi
 
-
-CLONE_DIR=$(mktemp -d)
-
 echo "[+] Git version"
 git --version
 
@@ -65,50 +62,18 @@ echo "[+] Cloning destination git repository $DESTINATION_REPOSITORY_NAME"
 git config --global user.email "$USER_EMAIL"
 git config --global user.name "$USER_NAME"
 
-{
-	git clone --single-branch --depth 1 --branch "$TARGET_BRANCH" "$GIT_CMD_REPOSITORY" "$CLONE_DIR"
-} || {
-	echo "::error::Could not clone the destination repository. Command:"
-	echo "::error::git clone --single-branch --branch $TARGET_BRANCH $GIT_CMD_REPOSITORY $CLONE_DIR"
-	echo "::error::(Note that if they exist USER_NAME and API_TOKEN is redacted by GitHub)"
-	echo "::error::Please verify that the target repository exist AND that it contains the destination branch name, and is accesible by the API_TOKEN_GITHUB OR SSH_DEPLOY_KEY"
-	exit 1
-
-}
-ls -la "$CLONE_DIR"
-
-
-
 echo "[+] Listing Current Directory Location"
 ls -al
+echo "[+] Show current origin"
+git remote -v
+echo "[+] Delete origin"
+git remote rm origin
+echo "[+] Show current origin"
+git remote -v
+echo "[+] Add origin"
+git remote add destination_origin $GIT_CMD_REPOSITORY
+echo "[+] Show current origin"
+git remote -v
 
-rm -rf ".git"
-
-cp -ra "$SOURCE_DIRECTORY"/. "$CLONE_DIR/"
-cd "$CLONE_DIR"
-
-echo "[+] Files that will be pushed"
-ls -la
-
-ORIGIN_COMMIT="https://$GITHUB_SERVER/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
-COMMIT_MESSAGE="${COMMIT_MESSAGE/ORIGIN_COMMIT/$ORIGIN_COMMIT}"
-COMMIT_MESSAGE="${COMMIT_MESSAGE/\$GITHUB_REF/$GITHUB_REF}"
-
-echo "[+] Set directory is safe ($CLONE_DIR)"
-# Related to https://github.com/cpina/github-action-push-to-another-repository/issues/64 and https://github.com/cpina/github-action-push-to-another-repository/issues/64
-# TODO: review before releasing it as a version
-git config --global --add safe.directory "$CLONE_DIR"
-
-echo "[+] Adding git commit"
-git add .
-
-echo "[+] git status:"
-git status
-
-echo "[+] git diff-index:"
-# git diff-index : to avoid doing the git commit failing if there are no changes to be commit
-git diff-index --quiet HEAD || git commit --message "$COMMIT_MESSAGE"
-
-echo "[+] Pushing git commit"
-# --set-upstream: sets de branch when pushing to a branch that does not exist
-git push "$GIT_CMD_REPOSITORY" --set-upstream "$TARGET_BRANCH"
+echo "[+] Pushing git commits"
+git push destination_origin $TARGET_BRANCH
